05bae2c73f9765c45cfb6b0a6aa90156db0ab095
Meng Wang <mengwangk@gmail.com>
2021-01-20 23:16:10 +0800

    Initial commit

--- grabql-api/scraper.js
+++ grabql-api/scraper.js
@@ -1,27 +1,27 @@
 import pdfHandler from "./libs/pdf-handler-lib";
-// import imageHandler from "./libs/image-handler-lib";
+import pngHandler from "./libs/png-handler-lib";
 import chromium from "chrome-aws-lambda";
 
 export const pdfScraper = pdfHandler(async (event, context) => {
-  if (
-    !event.queryStringParameters ||
-    !event.queryStringParameters.url ||
-    event.queryStringParameters.url.length === 0
-  ) {
-    throw new Error("Please supply a valid URL");
-  }
-  const params = {
-    url: event.queryStringParameters.url,
-    width: Number.parseInt(event.queryStringParameters.width) || 1920,
-    height: Number.parseInt(event.queryStringParameters.height) || 1080,
-  };
-
-  if (!params.url.startsWith("http")) {
-    params.url = "https://" + params.url;
-  }
-
   let browser = null;
   try {
+    if (
+      !event.queryStringParameters ||
+      !event.queryStringParameters.url ||
+      event.queryStringParameters.url.length === 0
+    ) {
+      throw new Error("Please supply a valid URL");
+    }
+    const params = {
+      url: event.queryStringParameters.url,
+      width: Number.parseInt(event.queryStringParameters.width) || 1920,
+      height: Number.parseInt(event.queryStringParameters.height) || 1080,
+    };
+
+    if (!params.url.startsWith("http")) {
+      params.url = "https://" + params.url;
+    }
+
     browser = await chromium.puppeteer.launch({
       headless: chromium.headless,
       args: chromium.args,
@@ -34,7 +34,6 @@ export const pdfScraper = pdfHandler(async (event, context) => {
     if (params.width > 0 && params.height > 0) {
       await webpage.setViewport({ width: params.width, height: params.height });
     }
-
     await webpage.goto(params.url, { waitUntil: "networkidle0" });
     const pdf = await webpage.pdf({
       printBackground: true,
@@ -47,6 +46,54 @@ export const pdfScraper = pdfHandler(async (event, context) => {
       },
     });
     return pdf;
+  } catch (error) {
+    throw new Error(error.message);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+});
+
+export const pngScraper = pngHandler(async (event, context) => {
+  let browser = null;
+  try {
+    if (
+      !event.queryStringParameters ||
+      !event.queryStringParameters.url ||
+      event.queryStringParameters.url.length === 0
+    ) {
+      throw new Error("Please supply a valid URL");
+    }
+    const params = {
+      url: event.queryStringParameters.url,
+      width: Number.parseInt(event.queryStringParameters.width) || 1920,
+      height: Number.parseInt(event.queryStringParameters.height) || 1080,
+    };
+
+    if (!params.url.startsWith("http")) {
+      params.url = "https://" + params.url;
+    }
+
+    browser = await chromium.puppeteer.launch({
+      headless: chromium.headless,
+      args: chromium.args,
+      ignoreHTTPSErrors: true,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+    });
+
+    const webpage = await browser.newPage();
+    if (params.width > 0 && params.height > 0) {
+      await webpage.setViewport({ width: params.width, height: params.height });
+    }
+    await webpage.goto(params.url, { waitUntil: "networkidle0" });
+
+    console.log(params);
+    const png = await webpage.screenshot({
+      fullPage: true,
+    });
+    return png;
   } catch (error) {
     throw new Error(error.message);
   } finally {
