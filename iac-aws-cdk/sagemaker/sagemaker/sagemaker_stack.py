2175f89c2941bb2867fcd4ae029ffbe27e697c42
Meng Wang <mengwangk@gmail.com>
2021-06-28 20:33:53 +0800

    Initial commit

--- iac-aws-cdk/sagemaker/sagemaker/sagemaker_stack.py
+++ iac-aws-cdk/sagemaker/sagemaker/sagemaker_stack.py
@@ -16,3 +16,69 @@ class SagemakerStack(core.Stack):
         topic = sns.Topic(self, "SagemakerTopic")
 
         topic.add_subscription(subs.SqsSubscription(queue))
+
+        # -----
+
+        # VPC
+        self.vpc = ec2.Vpc(
+            self,
+            "VPC",
+            max_azs=2,
+            cidr="10.10.0.0/16",
+            subnet_configuration=[
+                ec2.SubnetConfiguration(subnet_type=ec2.SubnetType.PUBLIC,
+                                        name="Public",
+                                        cidr_mask=24),
+                ec2.SubnetConfiguration(subnet_type=ec2.SubnetType.PRIVATE,
+                                        name="Private",
+                                        cidr_mask=24)
+            ],
+            nat_gateways=1)
+
+        # Security group
+        # self.sg = ec2.SecurityGroup(self, "securityGroup", self.vpc)
+        self.sg = ec2.SecurityGroup.from_security_group_id(
+            self,
+            "securityGroup",
+            self.vpc.vpc_default_security_group,
+            mutable=False)
+
+        # Create S3 bucket
+        self.bucket = s3.Bucket(self,
+                                "alpha2phi_sm_notebook_s3_bucket",
+                                removal_policy=core.RemovalPolicy.DESTROY)
+
+        # IAM Roles
+        # Create role for Notebook instance
+        nRole = iam_.Role(self,
+                          "notebookAccessRole",
+                          assumed_by=iam_.ServicePrincipal('sagemaker'))
+
+        nPolicy = iam_.Policy(self,
+                              "notebookAccessPolicy",
+                              policy_name="notebookAccessPolicy",
+                              statements=[
+                                  iam_.PolicyStatement(
+                                      actions=['s3:*', 'sagemaker:*'],
+                                      resources=[
+                                          '*',
+                                      ]),
+                              ]).attach_to_role(nRole)
+
+        # Create notebook instances cluster
+        nid = 'CDK-Notebook-Instance-ML-1'
+        notebook = sm.CfnNotebookInstance(
+            self,
+            nid,
+            instance_type='ml.t2.medium',
+            volume_size_in_gb=5,
+            security_group_ids=[self.sg.security_group_id],
+            subnet_id=self.vpc.private_subnets[0].subnet_id,
+            notebook_instance_name=nid,
+            role_arn=nRole.role_arn)
+
+        core.CfnOutput(self, "VPC Id", value=self.vpc.vpc_id)
+        core.CfnOutput(self, "Bucket", value=self.bucket.bucket_arn)
+        core.CfnOutput(self,
+                       "Notebook instance name",
+                       value=notebook.notebook_instance_name)
