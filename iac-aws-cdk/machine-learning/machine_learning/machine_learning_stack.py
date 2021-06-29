3448af8fcd653ce2752a7682929fa00ef7570adb
Meng Wang <mengwangk@gmail.com>
2021-06-29 22:50:19 +0800

    Update stack

--- iac-aws-cdk/machine-learning/machine_learning/machine_learning_stack.py
+++ iac-aws-cdk/machine-learning/machine_learning/machine_learning_stack.py
@@ -4,26 +4,20 @@ from .constructs import MLEfs, MLLib, MLVpc
 
 
 class MachineLearningStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str,
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Create VPC
         vpc_construct = MLVpc(self, "ml-vpc")
 
         # Create EFS
-        efs_construct = MLEfs(
-            self,
-            "ml-efs",
-            vpc=vpc_construct.vpc
-        )
+        efs_construct = MLEfs(self, "ml-efs", vpc=vpc_construct.vpc)
 
         # Install machine learning libraries
-        lib_construct = MLLib(
-            self,
-            "ml-lib",
-            vpc=vpc_construct.vpc,
-            ec2_instance_type="t2.micro",
-            efs_share=efs_construct.efs_share,
-            efs_sg=efs_construct.efs_sg
-        )
+        lib_construct = MLLib(self,
+                              "ml-lib",
+                              vpc=vpc_construct.vpc,
+                              ec2_instance_type="t2.micro",
+                              efs_share=efs_construct.efs_share,
+                              efs_sg=efs_construct.efs_sg)
